6282ab7ada822246369ae342a41b75c92bd7152a
Anass <146187264+nasaou@users.noreply.github.com>
2025-10-24 12:24:04 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'ניצן ירושלמי - תיווך וליווי נדל״ן',
+  description: 'ניצן ירושלמי הוא מתווך נדל״ן מקצועי ובעל ניסיון רחב בשוק הישראלי והאמריקאי. הוא מתמחה במכירת והשכרת דירות בתל אביב ובמרכז הארץ, לצד ליווי משקיעים ברכישת נכסים בארצות הברית. האתר מציג את הסגנון האישי שלו – אמין, נגיש, מקצועי, ועם הבנה מעמיקה בשוק המקומי והבינלאומי.
+',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#8c8c8c" />
+      </head>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          {/* Navbar will be inserted here */}
+
+          {children}
+
+        </div>
+      </body>
+    </html>
+  );
+}
