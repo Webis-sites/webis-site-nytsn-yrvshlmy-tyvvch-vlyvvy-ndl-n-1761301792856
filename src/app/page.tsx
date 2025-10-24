f2c263f986c39bcaad881d4e0fb97d3078ed5359
Anass <146187264+nasaou@users.noreply.github.com>
2025-10-24 12:31:01 +0100

    Add ProcessSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import ServicesSection from '@/components/ServicesSection';
 import PortfolioSection from '@/components/PortfolioSection';
+import ProcessSection from '@/components/ProcessSection';
 
 export default function HomePage() {
   return (
@@ -16,6 +17,7 @@ export default function HomePage() {
         <AboutSection />
         <ServicesSection />
         <PortfolioSection />
+        <ProcessSection />
       </main>
     </>
   );
